3b2a75a3f0c60b159b15395cc5a8520678160b31
Your Name <you@example.com>
2020-11-18 11:05:54 +0000

    all files backup

--- hr_worked_days/__manifest__.py
+++ hr_worked_days/__manifest__.py
@@ -9,8 +9,8 @@
         Calculation of working days
         Calculates the number of working days for a selected month in the payslip calculation.
         Some use cases where this module can be useful:
-            *an employee hasn't worked the whole month and his basic pay needs to be prorated
-        based on the actual worked days out of total worked days
+            *an employee hasn't worked the whole month and his basic pay needs to be prorated based on the actual worked days out of total worked days
+            
             *use working days information for calculation in any other salary rule
         
         A float field has been added to the payslip form view which is calculating the working 
