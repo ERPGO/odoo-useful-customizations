1cc683d67e6ba22d92d088910288c252ccc1df9f
Your Name <you@example.com>
2020-11-19 11:54:37 +0000

    manifest and readme files ready please check

--- hr_worked_days/__manifest__.py
+++ hr_worked_days/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Dosttech Payslip Customizations",
-
-    'summary': """
-        Calculate working days excluding leaves in payslip""",
-
+    'version': '0.1',
+    'depends': ['base','hr_holidays','hr_payroll_community'],
+    'author': "ERPGO",
+    'category': 'Uncategorized',
+    'summary': """Calculate working days excluding leaves in payslip""",
     'description': """
         Calculation of working days
         Calculates the number of working days for a selected month in the payslip calculation.
@@ -15,26 +16,15 @@
         
         A float field has been added to the payslip form view which is calculating the working 
         days in respective working schedule of an employee by talking out the respective global
-        leaves.
-        
-    """,
-
-    'author': "ERPGO",
+        leaves.""",
     'website': "http://www.erpgo.az",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','hr_holidays','hr_payroll_community'],
-
     # always loaded
     'data': [
         'views/views.xml',
     ],
-    # only loaded in demonstration mode
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
     
 }
